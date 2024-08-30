b2371e4a17dfaf8b5a867d7b045e51ff365adbea
Musfiqur Rahman <musfiq552608@gmail.com>
2024-08-30 22:41:47 +0600

    JWT config

---
+++ src/main/java/org/freedu/securityprac/SecuritypracApplication.java
@@ -0,0 +1,13 @@
+package org.freedu.securityprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritypracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritypracApplication.class, args);
+    }
+
+}
